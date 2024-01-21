4e8f666b10e2b724568c0aaa1e7dd62c5bc38b66
moetaz <3ezo.yasser@gmail.com>
2024-01-20 20:28:01 -0500

    add movements

---
+++ Assets/Scripts/UDPReceive.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceive : MonoBehaviour
+{
+
+    Thread receiveThread;
+    UdpClient client; 
+    public int port = 5052;
+    public bool startRecieving = true;
+    public bool printToConsole = false;
+    public string data;
+    public string[] gestureMovementArr;
+
+
+    public void Start()
+    {
+
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+    }
+
+
+    // receive thread
+    private void ReceiveData()
+    {
+
+        client = new UdpClient(port);
+        while (startRecieving)
+        {
+
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] dataByte = client.Receive(ref anyIP);
+                data = Encoding.UTF8.GetString(dataByte);
+
+                //gestures can be: pointer, open, close, ok
+                //movement can be: clockwise, counter clockwise, move, stop
+                gestureMovementArr = data.Split(',');
+
+                if (printToConsole) { print(gestureMovementArr[0] + " " + gestureMovementArr[1]); }
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+
+
+
+{
+    // Start is called before the first frame update
+    [SerializeField] private float speed;
+
+    private bool isUsingKeyboard;
+
+    [SerializeField] GameObject inputReceiver;
+
+    private UDPReceive inputRecieverComponent;
+
+    [SerializeField] private float moveSpeed = 3f; // Speed at which the player moves.
+    Rigidbody2D body;
+
+    private void Awake()
+    {
+       
+        Console.WriteLine(inputReceiver);
+        body = GetComponent<Rigidbody2D>();
+        body.gravityScale = 0;
+        inputRecieverComponent = inputReceiver.GetComponent<UDPReceive>();
+        isUsingKeyboard = true;
+       
+
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //body.velocity = new Vector2(body.velocity.x, Input.GetAxis("Vertical") * speed);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            isUsingKeyboard = !isUsingKeyboard;
+        }
+        if (isUsingKeyboard)
+        {
+            // Handle keyboard events
+            HandleKeyboardInputs();
+        }
+        else
+        {
+            // Handle hand gestures 
+            HandleGesturesInputs();
+        }
+    }
+
+    private void HandleKeyboardInputs()
+    {
+        // Get input from the vertical axis (default is arrow keys or 'W' and 'S').
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the amount to move this frame.
+        // Time.deltaTime converts moveSpeed to units per second instead of units per frame.
+        float movementAmount = verticalInput * moveSpeed * Time.deltaTime;
+
+        // Move the player's transform.
+        transform.Translate(0, movementAmount, 0);
+    }
+
+    private void HandleGesturesInputs() {
+        // Down
+        int direction = 0;
+        float movementAmount = 0;
+        if (inputRecieverComponent.gestureMovementArr[0].Equals("Close")){
+            direction = -1;
+            movementAmount = direction * moveSpeed * Time.deltaTime;
+        }
+        else if (inputRecieverComponent.gestureMovementArr[0].Equals("Open"))
+        {
+            direction = 1;
+            movementAmount = direction * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+           
+        }
+        transform.Translate(0, movementAmount, 0);
+    }
+}
